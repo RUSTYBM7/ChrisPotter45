5dc87b90d8d0fa3b9206ef22936727151d3c4a0e
Heart bridge Org <215229024+RUSTYBM7@users.noreply.github.com>
2026-05-06 14:59:31 -0400

    Update HeroSocialBar.tsx

--- artifacts/chris-potter/src/components/shared/HeroSocialBar.tsx
+++ artifacts/chris-potter/src/components/shared/HeroSocialBar.tsx
@@ -63,7 +63,7 @@ export default function HeroSocialBar() {
 
       {/* Facebook */}
       <a
-        href="https://www.facebook.com/share/1JReFApieZ/?mibextid=wwXIfr"
+        href="https://www.facebook.com/profile.php?id=61589438727256&mibextid=wwXIfr&mibextid=wwXIfr"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Facebook"
